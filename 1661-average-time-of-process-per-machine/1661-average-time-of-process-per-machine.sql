684a9ade4b6968ced084be63baa0884b4eb02f12
Gyeongmin Chae <51178411+gyeonminchae@users.noreply.github.com>
2024-04-22 21:36:09 +0900

    Update 1661-average-time-of-process-per-machine.sql

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -5,3 +5,5 @@ JOIN activity act2
 ON (act1.machine_id = act2.machine_id) AND (act1.process_id = act2.process_id)
 WHERE act1.activity_type = 'start' AND act2.activity_type = 'end'
 GROUP BY machine_id;
+
+## ROUND, AVG, MINUS, ONE - TWO table 이용 => JOIN, WHERE, GROUP BY
